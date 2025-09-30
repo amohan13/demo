df03c4facef94f7aa4d11f44369b298ffb0e8b01
amohan13 <anandmohanca31@gmail.com>
2025-09-30 09:28:27 +0530

    changes added

--- demo/src/main/java/com/example/demo/Test.java
+++ demo/src/main/java/com/example/demo/Test.java
@@ -3,6 +3,9 @@ package com.example.demo;
 public class Test {
 
     public static void main(String[] args) {
+
+        System.out.println("test");
+        System.out.println("test");
         System.out.println("test");
     }
 }
